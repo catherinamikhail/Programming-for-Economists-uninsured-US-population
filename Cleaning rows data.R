5be2c4d92ba0ee855781b78bd522105596ee16e2
WiesPolderman <wiespolderman@gmail.com>
2025-06-05 13:35:39 +0200

    Update cleaning Rows & columns
    Here is the cleaning of the rows & columns for 2010-2023

---
+++ Cleaning rows data.R
@@ -0,0 +1,38 @@
+dataset_uninsured_population_2010 <- dataset_uninsured_population_2010[-c(2:120),]
+View(dataset_uninsured_population_2010)
+
+dataset_uninsured_population_2011 <- dataset_uninsured_population_2011[-c(2:120),]
+View(dataset_uninsured_population_2011)
+
+dataset_uninsured_population_2012 <- dataset_uninsured_population_2012[-c(2:120),]
+View(dataset_uninsured_population_2012)
+
+dataset_uninsured_population_2013 <- dataset_uninsured_population_2013[-c(2:120),]
+View(dataset_uninsured_population_2013)
+
+dataset_uninsured_population_2014 <- dataset_uninsured_population_2014[-c(2:120),]
+View(dataset_uninsured_population_2014)
+
+dataset_uninsured_population_2015 <- dataset_uninsured_population_2015[-c(2:120),]
+View(dataset_uninsured_population_2015)
+
+dataset_uninsured_population_2016 <- dataset_uninsured_population_2016[-c(2:120),]
+View(dataset_uninsured_population_2016)
+
+dataset_uninsured_population_2017 <- dataset_uninsured_population_2017[-c(2:120),]
+View(dataset_uninsured_population_2017)
+
+dataset_uninsured_population_2018 <- dataset_uninsured_population_2018[-c(2:120),]
+View(dataset_uninsured_population_2018)
+
+dataset_uninsured_population_2019 <- dataset_uninsured_population_2019[-c(2:120),]
+View(dataset_uninsured_population_2019)
+
+dataset_uninsured_population_2021 <- dataset_uninsured_population_2021[-c(2:120),]
+View(dataset_uninsured_population_2021)
+
+dataset_uninsured_population_2022 <- dataset_uninsured_population_2022[-c(2:120),]
+View(dataset_uninsured_population_2022)
+
+dataset_uninsured_population_2023 <- dataset_uninsured_population_2023[-c(2:120),]
+View(dataset_uninsured_population_2023)

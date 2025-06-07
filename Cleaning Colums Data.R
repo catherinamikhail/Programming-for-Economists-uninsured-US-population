3c0e0033441859fc919bcc1d95f7e103ecef50f2
catherinamikhail <c.mikhail2@student.vu.nl>
2025-06-07 15:31:44 +0200

    data cleaning uninsured population
    I have cleaned out most of the unnecessary metadata but the household income and the ratio of the poverty level as I am still hesitating to delete the latter one or that we should keep it. Besides that I have cleaned out the margin of errors and kept the total population in the columns. Besides that, I have stored my environment with the (almost) cleaned datasets of the uninsured population in the US of the years 2010-2023 except for year 2020

--- Cleaning Colums Data.R
+++ Cleaning Colums Data.R
@@ -38,3 +38,43 @@ View(dataset_uninsured_population_2022)
 
 dataset_uninsured_population_2023 = dataset_uninsured_population_2023[, which(!grepl("Noninstitutionalized", colnames(dataset_uninsured_population_2023)))]
 View(dataset_uninsured_population_2023)
+
+# This is me cleaning out the data insured population from 2010-2016 getting out the margin of error
+dataset_uninsured_population_2010 = dataset_uninsured_population_2010[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2010)))]
+View(dataset_uninsured_population_2010)
+
+dataset_uninsured_population_2011 = dataset_uninsured_population_2011[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2011)))]
+View(dataset_uninsured_population_2011)
+
+dataset_uninsured_population_2012 = dataset_uninsured_population_2012[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2012)))]
+View(dataset_uninsured_population_2012)
+
+dataset_uninsured_population_2013 = dataset_uninsured_population_2013[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2013)))]
+View(dataset_uninsured_population_2013)
+
+dataset_uninsured_population_2014 = dataset_uninsured_population_2014[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2014)))]
+View(dataset_uninsured_population_2014)
+
+dataset_uninsured_population_2015 = dataset_uninsured_population_2015[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2015)))]
+View(dataset_uninsured_population_2015)
+
+dataset_uninsured_population_2016 = dataset_uninsured_population_2016[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2016)))]
+View(dataset_uninsured_population_2016)
+
+dataset_uninsured_population_2017 = dataset_uninsured_population_2017[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2017)))]
+View(dataset_uninsured_population_2017)
+
+dataset_uninsured_population_2018 = dataset_uninsured_population_2018[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2018)))]
+View(dataset_uninsured_population_2018)
+
+dataset_uninsured_population_2019 = dataset_uninsured_population_2019[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2019)))]
+View(dataset_uninsured_population_2019)
+
+dataset_uninsured_population_2021 = dataset_uninsured_population_2021[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2021)))]
+View(dataset_uninsured_population_2021)
+
+dataset_uninsured_population_2022 = dataset_uninsured_population_2022[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2022)))]
+View(dataset_uninsured_population_2022)
+
+dataset_uninsured_population_2023 = dataset_uninsured_population_2023[, which(!grepl("Margin of Error", colnames(dataset_uninsured_population_2023)))]
+View(dataset_uninsured_population_2023)

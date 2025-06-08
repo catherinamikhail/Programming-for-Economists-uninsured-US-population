80f76e2d3632107d29f3d68d5f3ef82d1a43a29e
WiesPolderman <wiespolderman@gmail.com>
2025-06-08 16:09:05 +0200

    Cleaning Missing data States
    All the states who miss 1+ year of data are excluded from the datasets. These states are: Delaware, Disctrict of Columbia, Hawaii, North Dakota, Puerto Rico, Rhode Island, Vermont and Wyoming.

---
+++ Data cleaning/Cleaning column Rhode Island.R
@@ -0,0 +1,38 @@
+dataset_uninsured_population_2010 = dataset_uninsured_population_2010[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2010)))]
+View(dataset_uninsured_population_2010)
+
+dataset_uninsured_population_2011 = dataset_uninsured_population_2011[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2011)))]
+View(dataset_uninsured_population_2011)
+
+dataset_uninsured_population_2012 = dataset_uninsured_population_2012[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2012)))]
+View(dataset_uninsured_population_2012)
+
+dataset_uninsured_population_2013 = dataset_uninsured_population_2013[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2013)))]
+View(dataset_uninsured_population_2013)
+
+dataset_uninsured_population_2014 = dataset_uninsured_population_2014[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2014)))]
+View(dataset_uninsured_population_2014)
+
+dataset_uninsured_population_2015 = dataset_uninsured_population_2015[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2015)))]
+View(dataset_uninsured_population_2015)
+
+dataset_uninsured_population_2016 = dataset_uninsured_population_2016[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2016)))]
+View(dataset_uninsured_population_2016)
+
+dataset_uninsured_population_2017 = dataset_uninsured_population_2017[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2017)))]
+View(dataset_uninsured_population_2017)
+
+dataset_uninsured_population_2018 = dataset_uninsured_population_2018[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2018)))]
+View(dataset_uninsured_population_2018)
+
+dataset_uninsured_population_2019 = dataset_uninsured_population_2019[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2019)))]
+View(dataset_uninsured_population_2019)
+
+dataset_uninsured_population_2021 = dataset_uninsured_population_2021[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2021)))]
+View(dataset_uninsured_population_2021)
+
+dataset_uninsured_population_2022 = dataset_uninsured_population_2022[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2022)))]
+View(dataset_uninsured_population_2022)
+
+dataset_uninsured_population_2023 = dataset_uninsured_population_2023[, which(!grepl("Rhode Island", colnames(dataset_uninsured_population_2023)))]
+View(dataset_uninsured_population_2023)

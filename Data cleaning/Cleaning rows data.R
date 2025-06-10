fa4ff654544a059442163fca6dd5746fd6c08f12
catherinamikhail <c.mikhail2@student.vu.nl>
2025-06-10 11:39:08 +0200

    putting the files in maps

--- Data cleaning/Cleaning rows data.R
+++ Data cleaning/Cleaning rows data.R
@@ -1,39 +1,78 @@
 #this is me cleaning out the rows with unnecessary metadata and keeping household income levels and the total population numbers
-dataset_uninsured_population_2010 <- dataset_uninsured_population_2010[-c(112:120),]
+dataset_uninsured_population_2010 <- dataset_uninsured_population_2010[-c(2:104),]
 View(dataset_uninsured_population_2010)
 
-dataset_uninsured_population_2011 <- dataset_uninsured_population_2011[-c(112:120),]
+dataset_uninsured_population_2011 <- dataset_uninsured_population_2011[-c(2:104),]
 View(dataset_uninsured_population_2011)
 
-dataset_uninsured_population_2012 <- dataset_uninsured_population_2012[-c(112:120),]
+dataset_uninsured_population_2012 <- dataset_uninsured_population_2012[-c(2:104),]
 View(dataset_uninsured_population_2012)
 
-dataset_uninsured_population_2013 <- dataset_uninsured_population_2013[-c(112:120),]
+dataset_uninsured_population_2013 <- dataset_uninsured_population_2013[-c(2:104),]
 View(dataset_uninsured_population_2013)
 
-dataset_uninsured_population_2014 <- dataset_uninsured_population_2014[-c(112:120),]
+dataset_uninsured_population_2014 <- dataset_uninsured_population_2014[-c(2:104),]
 View(dataset_uninsured_population_2014)
 
-dataset_uninsured_population_2015 <- dataset_uninsured_population_2015[-c(112:120),]
+dataset_uninsured_population_2015 <- dataset_uninsured_population_2015[-c(2:104),]
 View(dataset_uninsured_population_2015)
 
-dataset_uninsured_population_2016 <- dataset_uninsured_population_2016[-c(112:120),]
+dataset_uninsured_population_2016 <- dataset_uninsured_population_2016[-c(2:104),]
 View(dataset_uninsured_population_2016)
 
-dataset_uninsured_population_2017 <- dataset_uninsured_population_2017[-c(112:120),]
+dataset_uninsured_population_2017 <- dataset_uninsured_population_2017[-c(2:104),]
 View(dataset_uninsured_population_2017)
 
-dataset_uninsured_population_2018 <- dataset_uninsured_population_2018[-c(112:120),]
+dataset_uninsured_population_2018 <- dataset_uninsured_population_2018[-c(2:104),]
 View(dataset_uninsured_population_2018)
 
-dataset_uninsured_population_2019 <- dataset_uninsured_population_2019[-c(112:120),]
+dataset_uninsured_population_2019 <- dataset_uninsured_population_2019[-c(2:104),]
 View(dataset_uninsured_population_2019)
 
-dataset_uninsured_population_2021 <- dataset_uninsured_population_2021[-c(112:120),]
+dataset_uninsured_population_2021 <- dataset_uninsured_population_2021[-c(2:104),]
 View(dataset_uninsured_population_2021)
 
-dataset_uninsured_population_2022 <- dataset_uninsured_population_2022[-c(112:120),]
+dataset_uninsured_population_2022 <- dataset_uninsured_population_2022[-c(2:104),]
 View(dataset_uninsured_population_2022)
 
-dataset_uninsured_population_2023 <- dataset_uninsured_population_2023[-c(112:120),]
+dataset_uninsured_population_2023 <- dataset_uninsured_population_2023[-c(2:104),]
+View(dataset_uninsured_population_2023)
+
+dataset_uninsured_population_2010 <- dataset_uninsured_population_2010[-c(10:17),]
+View(dataset_uninsured_population_2010)
+
+dataset_uninsured_population_2011 <- dataset_uninsured_population_2011[-c(10:17),]
+View(dataset_uninsured_population_2011)
+
+dataset_uninsured_population_2012 <- dataset_uninsured_population_2012[-c(10:17),]
+View(dataset_uninsured_population_2012)
+
+dataset_uninsured_population_2013 <- dataset_uninsured_population_2013[-c(10:17),]
+View(dataset_uninsured_population_2013)
+
+dataset_uninsured_population_2014 <- dataset_uninsured_population_2014[-c(10:17),]
+View(dataset_uninsured_population_2014)
+
+dataset_uninsured_population_2015 <- dataset_uninsured_population_2015[-c(10:17),]
+View(dataset_uninsured_population_2015)
+
+dataset_uninsured_population_2016 <- dataset_uninsured_population_2016[-c(10:17),]
+View(dataset_uninsured_population_2016)
+
+dataset_uninsured_population_2017 <- dataset_uninsured_population_2017[-c(10:17),]
+View(dataset_uninsured_population_2017)
+
+dataset_uninsured_population_2018 <- dataset_uninsured_population_2018[-c(10:17),]
+View(dataset_uninsured_population_2018)
+
+dataset_uninsured_population_2019 <- dataset_uninsured_population_2019[-c(10:17),]
+View(dataset_uninsured_population_2019)
+
+dataset_uninsured_population_2021 <- dataset_uninsured_population_2021[-c(10:17),]
+View(dataset_uninsured_population_2021)
+
+dataset_uninsured_population_2022 <- dataset_uninsured_population_2022[-c(10:17),]
+View(dataset_uninsured_population_2022)
+
+dataset_uninsured_population_2023 <- dataset_uninsured_population_2023[-c(10:17),]
 View(dataset_uninsured_population_2023)

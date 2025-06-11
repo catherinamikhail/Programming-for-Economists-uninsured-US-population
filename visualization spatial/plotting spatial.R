6aac41285ba6432cf9579d4f4dce45ac70056d70
catherinamikhail <c.mikhail2@student.vu.nl>
2025-06-11 13:18:53 +0200

    plotting again the spatial visualization

--- visualization spatial/plotting spatial.R
+++ visualization spatial/plotting spatial.R
@@ -1,9 +1,9 @@
 #Here I am trying to plot the spatial subset
 
 library(ggplot2)
-
-ggplot(us_map, aes(x = long, y =lat, group = group)) + geom_polygon(aes(fill = Uninsured_share.x)) + 
-  scale_fill_gradient(low = "lightblue", high = "red") + ggtitle("Share of Uninsured Population US, 2023") + guides(fill = guide_colorbar(title = "Percentage Share Uninsured"))
+library(dplyr)
+ggplot(us_map, aes(x = long, y =lat, group = group)) + geom_polygon(aes(fill = value)) + 
+  scale_fill_gradient(low = "lightblue", high = "red", na.value = "white") + ggtitle("Share of Uninsured Population US, 2023") + guides(fill = guide_colorbar(title = "Percentage Share Uninsured"))
 
 match(us_map$region, spatial_df$state)
 
@@ -11,3 +11,4 @@ us_map$region = tolower(trimws(us_map$region))
 spatial_df$state = tolower(trimws(spatial_df$state))
 
 us_map = left_join(us_map, spatial_df, by = c("region" = "state"))
+

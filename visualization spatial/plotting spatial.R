252f9da8b3c57821a54ddefc2f0a5df97f98f8f8
catherinamikhail <c.mikhail2@student.vu.nl>
2025-06-14 10:40:04 +0200

    also plotting alaska

--- visualization spatial/plotting spatial.R
+++ visualization spatial/plotting spatial.R
@@ -22,4 +22,35 @@ ggplot(us_map, aes(x = long, y = lat, group = group, fill = value)) +
   scale_fill_gradient(low = "lightblue", high = "red", na.value = "white")+
   ggtitle("Share of Uninsured Population in the US, 2023")+
   labs(fill = "(%) Share of Uninsured Population") + 
-  theme_void()
+  theme_void() 
+
+#I still need the state Alaska but the package maps does not include the state Alaska
+
+# I am trying it here again including Alaska
+
+library(rnaturalearth)
+library(ggplot2)
+library(dplyr)
+
+#get US shapefile
+us_states = ne_states(country = "United States of America", returnclass = "sf")
+
+#convert state names into lowercase letters such that they match
+us_states$name = tolower(us_states$name)
+
+#merge data with shapefile
+us_states_data = us_states %>%
+  left_join(spatial_df, by = c("name" = "state"))
+
+#plot the map with the data
+ggplot(us_states_data) + 
+  geom_sf(aes(fill = value)) + 
+  theme_void() + 
+  scale_fill_gradient( low = "lightblue", high = "darkred", na.value = "white") + 
+  ggtitle("Share of Uninsured Population in the US, 2023") +
+  labs(fill = "(%) Share of Uninsured Population") + 
+  theme(legend.position = "bottom",legend.key.height = unit(0.5, "cm"),legend.justification = "left",plot.margin = unit(c(0.5,1,1,2), "cm"), plot.title = element_text(margin = margin(b = 25)), aspect.ratio = 0.3) +
+  coord_sf(expand = F)
+  
+
+
